a4063f56faf9532d214275dcc742d3729830b90a
danielgolden <danielgolden90@gmail.com>
2022-02-14 11:21:46 -0500

    fix: check more false positives

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ export default function retextCapitalization() {
     search(tree, list, (match, index, parent, phrase) => {
       const actual = toString(match)
       let expected = schema[phrase]
+      let checkForFalsePositive = ''
 
       // The search function performs a fuzzy search on the string in question
       // ignoring casing, apostrophes, etc. This is a check too make sure
@@ -46,25 +47,54 @@ export default function retextCapitalization() {
         expected = [expected]
       }
 
-      // TODO: If "Observability" is a part of an accepted phrase like
-      // "Instant Observability" don't throw an error.
+      checkForFalsePositive = (actualToCheck, numberOfWordsBefore, matchesToCheck) => {
+        const matchesIsString = typeof matchesToCheck === 'string'
+        const matchesIsArray = Array.isArray(matchesToCheck)
 
-      if (actual === 'Observability') {
-        // Make a new array that includes the word before "Observability"
-        // and the word "Observability itself". Store it in `contextOfActual`
-        let contextOfActual = parent.children.map((item, index, array) => {
-            if (item === match[0]) {
-                return [array[index -2], array[index -1], item]
-            } else {
-                return false
-            }
+        if (!matchesIsString && !matchesIsArray) {
+          console.error(`"checkContext" expects the argument "matchesToCheck" to be an array or string'`)
+          console.log(typeof matchesToCheck)
+          return null
+        }
+
+        if (actualToCheck !== actual) {
+          return null
+        }
+
+        let actualWithContext = parent.children.map((item, index, array) => {
+          // if you've found the element
+          if (item === match[0]) {
+            let contextArray = []
+            let dummyArray = [...Array(numberOfWordsBefore * 2)]
+
+            // return an array of the words before it and it.
+            dummyArray.forEach((_, i) => {
+              contextArray.unshift(array[index -(i + 1)])
+            })
+
+            contextArray.push(item)
+
+            return contextArray
+          } else {
+              return false
+          }
         }).filter(item => item)[0]
 
-        if (toString(contextOfActual) === 'Instant Observability') {
-          return null
+        if (matchesIsString) {
+          return matchesToCheck === toString(actualWithContext)
+        } else if (matchesIsArray) {
+          return matchesToCheck.some(match => match === toString(actualWithContext))
         }
       }
 
+      // check for false positives 
+      if (
+        checkForFalsePositive('Observability', 1, 'Instant Observability')
+        checkForFalsePositive('I/O', 1, 'Relic I/O')
+      ) {
+        return null
+      }
+      
 
       Object.assign(
         file.message(
